b51384fb14892b7d593f01b055618002938ab2f1
sheraj97 <sherajnagorik@gmail.com>
2025-08-23 01:13:38 +0600

    Sherajulll

--- lib/Sheraj_one.dart
+++ lib/Sheraj_one.dart
@@ -1,6 +1,32 @@
-void main(){
+Future<void> main() async {
 
   // print("I need some time build myself");
   // print("This time is very cursel and struggel ");
+  print("Start");
+  // await task1();
+  var result=await Future.wait([task1(),task2(),task3()]);
+  print(result);
+  print("end");
+
+}
+
+
+Future<void> task1(){
+
+  String value='task-1';
+  return Future.delayed(Duration(seconds: 2),()=>value);
+
+}
+Future<void> task2(){
+
+  String value='task-2';
+  return Future.delayed(Duration(seconds: 3),()=>value);
+
+}
+
+Future<void> task3(){
+
+  String value='task-3';
+  return Future.delayed(Duration(seconds: 5),()=>value);
 
 }
